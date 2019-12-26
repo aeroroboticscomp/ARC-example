d5b034499b6aa64ca307a7ae136d94f80960f9e2
beldonl <beldonlin@gmail.com>
2019-12-26 17:20:24 -0500

    Produced introduction to Mini-Lessons

--- ARC_2019-2020/algo.py
+++ ARC_2019-2020/algo.py
@@ -2,7 +2,18 @@ import math
 
 def determinePath(waypoint):
 	# COMPLETE HERE
+	
 	order = [0,1,2,3,4]
+
+
+
+
+
+
+
+
+
+
 	return order
 
 
---
+++ Mini-Lessons/Algo_1/README.md
@@ -0,0 +1,2 @@
+# Mini-Lesson: Algorithm Task 1
+In this example, you'll be creating a programming task that allows you to calculate 
