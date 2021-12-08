b833968ea655111a692db485aa05c4fc9724ee6f
DonatoK <donatokava@gmail.com>
2021-12-07 18:05:38 -0700

    initial branching

--- ARC_2021-2022/main.py
+++ ARC_2021-2022/main.py
@@ -2,8 +2,9 @@ import math
 from algo import *
 from in_out import *
 
+
 def main(filename):
-    
+
     # Read in waypoints
     waypoints = readWaypointFile(filename)
 
@@ -11,9 +12,11 @@ def main(filename):
     path = determinePath(waypoints)
 
     # Export path
-    for i in path: # Print out order of Lat Long
-    	print("Latitude: "+str(waypoints['latitude'][i])+" Longitude: "+str(waypoints['longitude'][i]))
+    for i in path:  # Print out order of Lat Long
+    	print("Latitude: "+str(waypoints['latitude'][i])
+    	      + " Longitude: "+str(waypoints['longitude'][i]))
 
 
+#donato
 if __name__ == '__main__':
 	main("waypoint_1.txt")
